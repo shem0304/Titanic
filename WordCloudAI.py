4786dc90470d602e54a37d00111294e655437f17
shem0304 <shem0304@gmail.com>
2020-03-17 14:25:05 +0900

    2020.03.17 14:24

--- WordCloudAI.py
+++ WordCloudAI.py
@@ -1,12 +1,11 @@
 from collections import Counter
 from konlpy.tag import Twitter, Okt
 import pytagcloud
-import matplotlib.image as img
 import matplotlib.pyplot as pp
 from matplotlib import font_manager, rc
 import matplotlib
 import webbrowser
-import io
+import io, os
 from collections import OrderedDict
 
 #출처 : https://ericnjennifer.github.io/python_visualization/2018/01/21/PythonVisualization_Chapt3.html
@@ -15,41 +14,46 @@ from collections import OrderedDict
 title_text = []
 
 def showGraph(wordInfo):
-    font_location = "c:/Windows/fonts/malgun.ttf"
-    font_name = font_manager.FontProperties(fname=font_location).get_name()
-    matplotlib.rc('font', family=font_name)
+    currentPath = os.getcwd()
+    print(currentPath)
+    try :
+        font_location = "c:\\Windows\\fonts\\malgun.ttf"
+        font_name = font_manager.FontProperties(fname=font_location).get_name()
+        matplotlib.rc('font', family=font_name)
 
-    pp.title('울산 인공지능 뉴스 검색 결과')
-    pp.xlabel('주요 단어')
-    pp.ylabel('빈도수')
-    pp.grid(True)
+        pp.title('뉴스 검색 결과 빈도 분석')
+        pp.xlabel('주요 단어')
+        pp.ylabel('빈도수')
+        pp.grid(True)
 
-    Sorted_Dict_Values = sorted(wordInfo.values(), reverse=True)
-    Sorted_Dict_Keys = sorted(wordInfo, key=wordInfo.get, reverse=True)
+        Sorted_Dict_Values = sorted(wordInfo.values(), reverse=True)
+        Sorted_Dict_Keys = sorted(wordInfo, key=wordInfo.get, reverse=True)
 
-    pp.bar(range(len(wordInfo)), Sorted_Dict_Values, align='center')
-    pp.xticks(range(len(wordInfo)), list(Sorted_Dict_Keys), rotation='70')
+        pp.bar(range(len(wordInfo)), Sorted_Dict_Values, align='center')
+        pp.xticks(range(len(wordInfo)), list(Sorted_Dict_Keys), rotation='70')
 
-    pp.show()
+        pp.show()
+    except Exception as e:
+        print('Exception Error.', e)
 
 # 출처 : https://thinkwarelab.wordpress.com/2016/08/30/%ED%8C%8C%EC%9D%B4%EC%8D%AC-%ED%98%95%ED%83%9C%EC%86%8C-%EB%B6%84%EC%84%9D%EC%9C%BC%EB%A1%9C-%EC%9B%8C%EB%93%9C%ED%81%B4%EB%9D%BC%EC%9A%B0%EB%93%9C-%EA%B7%B8%EB%A6%AC%EA%B8%B0/
 
 def saveWordCloud(wordInfo, filename):
     taglist = pytagcloud.make_tags(dict(wordInfo).items(), maxsize=100)
-    pytagcloud.create_tag_image(taglist, filename, size=(640, 480), fontname='korean', rectangular=False)
+    pytagcloud.create_tag_image(taglist, filename, size=(700, 480), fontname='korean', rectangular=False)
     webbrowser.open(filename)
 
 def DrawGrap(atags):
 
     post_dict = OrderedDict() #OrderedDict를 사용하여, key에 url 입력
     text = io.open('d:\\aihhi.txt', 'a', encoding="utf-8")
-    text2 = io.open('D:\\articleinfo', 'a', encoding="utf-8")
+    #text2 = io.open('D:\\articleinfo', 'a', encoding="utf-8")
     for tag in atags:
         if tag['href'] in post_dict:  #현재 저장할 링크(key)가 이미 post_dict에 있으면
             break                     #작업종료
         print(tag.text)
         text.write(tag.text)
-        text2.write(str(len(post_dict))+', ' + tag.text+',  '+tag['href']+'\n\n')
+        #text2.write(str(len(post_dict))+', ' + tag.text+',  '+tag['href']+'\n\n')
         post_dict[tag['href']] = tag.text
         print('%s===> ' % len(post_dict))
     text.close()
